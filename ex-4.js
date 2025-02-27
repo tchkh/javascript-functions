ac0c0be1017748a3f9b07c30024f3c40f05cfa2b
Binwaran <binwaran.m@gmail.com>
2025-02-27 15:02:08 +0800

    Add: Exercise 1-6

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,27 @@
 // Start coding here
-let calculator = {};
+
+let add =(a,b) => a+b;
+    console.log(add(10,5));
+
+let subtract =(a,b) => a-b;
+    console.log(subtract(10,5));
+
+let multiply =(a,b) => a*b;
+    console.log(multiply(10,5));
+
+let divide =(a,b) => a/b;
+    console.log(divide(10,5));
+    
+// Start coding here
+let calculator = {
+    add: (a,b) => a+b,
+    subtract: (a,b) => a-b,
+    multiply: (a,b) => a*b,
+    divide: (a,b) => a/b,
+};
+
+let addResult = calculator.add(10,20);
+console.log(addResult);
+
+let divideResult = calculator.divide(3000,10)
+console.log(divideResult)
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,11 @@
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
+
+function findMinNumber(a,b) {
+    return Math.min(a,b);
+    
+}
+
+console.log(findMinNumber(10,5)); 
+
